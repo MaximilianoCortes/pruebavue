a1a3a73886cf711cb9771ddcf5643d4a4895bcd5
MaximilianoCortes <89611132+MaximilianoCortes@users.noreply.github.com>
2023-05-23 14:43:37 -0400

    proyecto inicializado

---
+++ OneDrive/Documentos/GitHub/prueba3isoft/src/main/java/com/ufro/prueba3isoft/Prueba3isoftApplication.java
@@ -0,0 +1,13 @@
+package com.ufro.prueba3isoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prueba3isoftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Prueba3isoftApplication.class, args);
+    }
+
+}
---
+++ OneDrive/Documentos/GitHub/prueba3isoft/src/test/java/com/ufro/prueba3isoft/Prueba3isoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufro.prueba3isoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prueba3isoftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
